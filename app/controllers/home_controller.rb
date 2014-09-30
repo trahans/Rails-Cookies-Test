6f9e35a210486ecede5da92462b64c4883d8051a
Stephan Trahan <stephan.trahan@gmail.com>
2014-09-30 14:08:44 -0400

    can now dynamically store product ids as cookies

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
+  	#Create a new cookie with the key-value pair :developer name, "Stephan"
   	cookies[:developer_name] = "Stephan"
+  	add_to_cart(1)
+  	add_to_cart(2)
+  end
+
+  def add_to_cart(product_id)
+  	cookies["product#{product_id}".to_sym] = product_id.to_s
   end
 end
